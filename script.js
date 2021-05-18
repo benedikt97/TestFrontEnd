2d106583114e1cbfc8855ea1b441961c0b5c69cf
benedikt97 <Heuser@live.de>
2021-05-18 12:12:19 +0200

    Implemented all functions in FrontEnd

--- script.js
+++ script.js
@@ -1,85 +1,95 @@
-const html = document.documentElement;
-const body = document.body;
-const menuLinks = document.querySelectorAll(".admin-menu a");
-const collapseBtn = document.querySelector(".admin-menu .collapse-btn");
-const toggleMobileMenu = document.querySelector(".toggle-mob-menu");
-const switchInput = document.querySelector(".switch input");
-const switchLabel = document.querySelector(".switch label");
-const switchLabelText = switchLabel.querySelector("span:last-child");
-const collapsedClass = "collapsed";
-const lightModeClass = "light-mode";
-
-/*TOGGLE HEADER STATE*/
-collapseBtn.addEventListener("click", function () {
-  body.classList.toggle(collapsedClass);
-  this.getAttribute("aria-expanded") == "true"
-    ? this.setAttribute("aria-expanded", "false")
-    : this.setAttribute("aria-expanded", "true");
-  this.getAttribute("aria-label") == "collapse menu"
-    ? this.setAttribute("aria-label", "expand menu")
-    : this.setAttribute("aria-label", "collapse menu");
-});
-
-/*TOGGLE MOBILE MENU*/
-toggleMobileMenu.addEventListener("click", function () {
-  body.classList.toggle("mob-menu-opened");
-  this.getAttribute("aria-expanded") == "true"
-    ? this.setAttribute("aria-expanded", "false")
-    : this.setAttribute("aria-expanded", "true");
-  this.getAttribute("aria-label") == "open menu"
-    ? this.setAttribute("aria-label", "close menu")
-    : this.setAttribute("aria-label", "open menu");
-});
-
-/*SHOW TOOLTIP ON MENU LINK HOVER*/
-for (const link of menuLinks) {
-  link.addEventListener("mouseenter", function () {
-    if (
-      body.classList.contains(collapsedClass) &&
-      window.matchMedia("(min-width: 768px)").matches
-    ) {
-      const tooltip = this.querySelector("span").textContent;
-      this.setAttribute("title", tooltip);
-    } else {
-      this.removeAttribute("title");
-    }
-  });
-}
-
-/*TOGGLE LIGHT/DARK MODE*/
-if (localStorage.getItem("dark-mode") === "false") {
-  html.classList.add(lightModeClass);
-  switchInput.checked = false;
-  switchLabelText.textContent = "Light";
-}
-
-switchInput.addEventListener("input", function () {
-  html.classList.toggle(lightModeClass);
-  if (html.classList.contains(lightModeClass)) {
-    switchLabelText.textContent = "Light";
-    localStorage.setItem("dark-mode", "false");
-  } else {
-    switchLabelText.textContent = "Dark";
-    localStorage.setItem("dark-mode", "true");
-  }
-});
 
 
 /*JQUERY Code */
 
 $(document).ready(function(){
   
-  $("#btSettings").click(function(){
-    $("#LiveMonitor").hide();
+  $("#btStartLipe").click(function(){
+    $.ajax({
+      url: "/api/",
+      data: { c: 'initopc'},
+      dataType: 'json',
+      success: function (data) {
+        
+      }
+    })
   });
+
+  $("#btStopLipe").click(function(){
+    $.ajax({
+      url: "/api/",
+      data: { c: 'stopopc'},
+      dataType: 'json',
+      success: function (data) {
+        
+      }
+    })
+  });
+
+  $("#btDeleteLog").click(function(){
+    $.ajax({
+      url: "/api/",
+      data: { c: 'deleteserverlog'},
+      dataType: 'json',
+      success: function (data) {
+        
+      }
+    })
+  });
+
+  $.makeTable = function (mydata) {
+    var table = $('<table border=1>');
+    var tblHeader = "<tr>";
+    for (var k in mydata[0]) tblHeader += "<th>" + k + "</th>";
+    tblHeader += "</tr>";
+    $(tblHeader).appendTo(table);
+    $.each(mydata, function (index, value) {
+        var TableRow = "<tr>";
+        $.each(value, function (key, val) {
+            TableRow += "<td>" + val + "</td>";
+        });
+        TableRow += "</tr>";
+        $(table).append(TableRow);
+    });
+    return ($(table));
+  };
+
+
+
+  $.makeAjaxRequest = function () {
 
     $.ajax({
       url: "/api/",
       data: { c: 'getopcconfig'},
       dataType: 'json',
       success: function (data) {
+        $("#opcconfiguration li").remove();
         $.each(data, function(key,value){
           $("#opcconfiguration").append("<li>" +key +": " +value +"</li>");
+        });
+      }
+    })
+
+    $.ajax({
+      url: "/api/",
+      data: { c: 'getstaticvalues'},
+      dataType: 'json',
+      success: function (data) {
+        $("#staticvalues li").remove();
+        $.each(data, function(key,value){
+          $("#staticvalues").append("<li>" +key +": " +value +"</li>");
+        });
+      }
+    })
+
+    $.ajax({
+      url: "/api/",
+      data: { c: 'getopclogs'},
+      dataType: 'json',
+      success: function (data) {
+        $("#opclogs li").remove();
+        $.each(data, function(key,value){
+          $("#opclogs").append("<li>" +key +": " +value +"</li>");
         });
       }
     })
@@ -89,11 +99,40 @@ $(document).ready(function(){
       data: { c: 'getnodes'},
       dataType: 'json',
       success: function (data) {
+        $("#watchednodes li").remove();
         $.each(data, function(key,value){
           $("#watchednodes").append("<li>" +key +": " +value +"</li>");
         });
       }
     })
+
+    $.ajax({
+      url: "/api/",
+      data: { c: 'getactvalues'},
+      dataType: 'json',
+      success: function (data) {
+        $("#actmachinedata li").remove();
+        $.each(data, function(key,value){
+          $("#actmachinedata").append("<li>" +key +": " +value +"</li>");
+        });
+      }
+    })
+
+    $.ajax({
+      url: "/api/",
+      data: { c: 'getlogs'},
+      dataType: 'json',
+      success: function (data) {
+        var table = $.makeTable(data);
+        $("#serverlog table").remove();
+        $(table).appendTo("#serverlog");
+        
+      }
+    })
+  
+  }
+
+  setInterval($.makeAjaxRequest, 1000)
   
   
 });
